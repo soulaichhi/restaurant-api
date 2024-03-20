c2d5a85afb81d30f8991b848acf05e8324c90277
soulaichhi <chhibou.soulaimane@gmail.com>
2024-03-20 15:46:15 +0000

    initial commit

---
+++ src/main/java/com/soulaichhi/onlinefoodorderingapi/OnlineFoodOrderingApiApplication.java
@@ -0,0 +1,13 @@
+package com.soulaichhi.onlinefoodorderingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soulaichhi/onlinefoodorderingapi/OnlineFoodOrderingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulaichhi.onlinefoodorderingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
