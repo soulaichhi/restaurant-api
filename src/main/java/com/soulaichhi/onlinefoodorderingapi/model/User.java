1cf29c8a7dba1a6609005d346312ad6f1cddef88
soulaichhi <chhibou.soulaimane@gmail.com>
2024-03-22 15:18:26 +0000

    Hide password on get profile #09

--- src/main/java/com/soulaichhi/onlinefoodorderingapi/model/User.java
+++ src/main/java/com/soulaichhi/onlinefoodorderingapi/model/User.java
@@ -1,6 +1,7 @@
 package com.soulaichhi.onlinefoodorderingapi.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.soulaichhi.onlinefoodorderingapi.dto.RestaurantDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -20,6 +21,7 @@ public class User {
     private Long id;
     private String fullName;
     private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private USER_ROLE role=USER_ROLE.ROLE_CUSTOMER;
     @JsonIgnore
